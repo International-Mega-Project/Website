132baa838af631d9167921648739df1b5cf964fa
JordiVanheeswyck <Jordi.Vanheeswyck@student.pxl.be>
2021-03-31 16:56:24 +0200

    openweatherAPI still buggy

--- public/src/js/map.js
+++ public/src/js/map.js
@@ -1,4 +1,10 @@
+//TODO: const axios = require('axios');
+
 mapboxgl.accessToken = 'pk.eyJ1IjoicnVhbmp2djIzIiwiYSI6ImNra3pzdnNjNDBtcm4ycHFvcGticGxnNmgifQ.ycPq0Fz2eyZlaRgTle9NQg';
+
+let lat;
+let long;
+
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
@@ -17,6 +23,24 @@ map.addControl(geocoder);
 
 geocoder.on('results', function (response) {
     var  array = response.request.response.body.features;
+    var coordinates = array[0].geometry.coordinates;
+    lat = coordinates[0];
+    long = coordinates[1];
     console.log(array[0].place_name);
     document.getElementById('mapformaddress').innerHTML = array[0].place_name;
 })
+
+function getWeatherDataButtonClick() {
+    //TODO: axios is not defined because i can't "require" it at line 1?
+    fetch({
+        method: 'GET',
+        url: 'https://v1.nocodeapi.com/jordivhw/ow/plqXjzrWGqJksYSi/byGeoCord/threeHourForecast',
+        params: {lat: lat,long: long},
+    }).then(function (response) {
+        // handle success
+        console.log(response.data);
+    }).catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+}
---
+++ public/src/js/weatherApi.js
@@ -0,0 +1,17 @@
+
+let long = '51.094791';
+let lat ='5.793970';
+
+const axios = require('axios');
+
+axios({
+    method: 'get',
+    url: 'https://v1.nocodeapi.com/jordivhw/ow/tsTdEvkyScufqCGI/byGeoCord/threeHourForecast',
+    params: {lat: lat,long: long},
+}).then(function (response) {
+    // handle success
+    console.log(response.data);
+}).catch(function (error) {
+    // handle error
+    console.log(error);
+})
