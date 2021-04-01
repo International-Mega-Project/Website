e4e817eed5b213917322454d6a6d78811813dcaa
Matin De Wet <576597@student.belgiumcampus.ac.za>
2021-04-01 17:11:09 +0200

    further implementation

--- public/src/js/OpenUvApi.js
+++ public/src/js/OpenUvApi.js
@@ -1,10 +1,6 @@
-function getInformation() {
-    console.log("Lat: " + document.getElementById('mapLat').innerHTML + "  Long: " + document.getElementById('mapLong').innerHTML);
+function getUVIndex() {
     var lat = document.getElementById('mapLat').innerHTML;
     var lng = document.getElementById('mapLong').innerHTML;
-    var alt = $('#alt').val();
-    var ozone = $('#ozone').val();
-    var dt = $('#dt').val();
 
     $.ajax({
         type: 'GET',
@@ -12,7 +8,7 @@ function getInformation() {
         beforeSend: function (request) {
             request.setRequestHeader('x-access-token', '6756ecb3ebdab1e6478f6c18028c83c0');
         },
-        url: 'https://api.openuv.io/api/v1/forecast?lat=' + lat + '&lng=' + lng + '&alt=' + alt + '&ozone=' + ozone + '&dt=' + dt,
+        url: 'https://api.openuv.io/api/v1/uv?lat=' + lat + '&lng=' + lng,
         success: function (response) {
             //handle successful response
         },
